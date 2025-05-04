85ac7140979d56db136e9f46cd87941c32793ab6
Kuntika Siengsukhon <atgenda@gmail.com>
2025-05-04 23:04:36 +0700

    feat: add answer in exervise

--- ex-2.js
+++ ex-2.js
@@ -2,5 +2,7 @@
 let numbers = [10, 20, 30, 40, 50];
 let total = 0;
 // Start coding here
-
+for (let price of numbers) {
+    total = total + price;
+}
 console.log(total);
--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,11 @@ let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
 
+for (let numbers of scores) {
+    if (minScore == undefined) {
+        minScore = numbers;
+    } else if (numbers < minScore) {
+        minScore = numbers;
+    };
+};
 console.log(minScore);
